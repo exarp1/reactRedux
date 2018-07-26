dc171f736b3964deb27a06debb2784f0753ff650
Richard O'Brien <richardobrien@Richards-iMac.local>
2018-07-26 04:51:48 -0700

    setup

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+
+// Libs
+const API_KEY = 'AIzaSyBmi5BCNY2AhhxiQJNrApIKI9Tlg_F8QtQ';
+
+// Components
+
+// Create a new component.
+// This component should produce some HTML
+const App = () => (
+  <div>
+    <SearchBar />
+  </div>
+);
+// make sure this component gets put into the DOM. ( ReactDOM.render())
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
